48e96bfb624f2c646c2d919d9622c61ac43e6675
Mark Raynsford <code@io7m.com>
2014-08-13 22:32:37 +0000

    Announce version on startup

--- io7m-jfprop-server/src/main/java/com/io7m/jfprop/JFPServerMain.java
+++ io7m-jfprop-server/src/main/java/com/io7m/jfprop/JFPServerMain.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -198,6 +198,8 @@ public final class JFPServerMain implements Runnable, JFPServerControlType
     this.logger = new JFPJettyLogger();
     this.log = this.logger.getMainLog();
     this.log.setLevel(in_config.getLogLevel());
+
+    this.log.info(JFPVersion.getVersionString() + " starting");
 
     final LogType log_fossil = this.log.with("fossil");
 
